2ca6c2f8bf613b61855b97dedeee1ef669f18f7b
gelicamarie <turla_angelica@yahoo.com>
2021-01-16 14:59:23 -0500

    icon links to other profiles : Jan.16

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { AiFillGithub } from '@react-icons/all-files/ai/AiFillGithub'
-import { AiFillLinkedin } from '@react-icons/all-files/ai/AiFillLinkedin'
-import { AiFillTwitterCircle } from '@react-icons/all-files/ai/AiFillTwitterCircle'
-import { AiOutlineMail } from '@react-icons/all-files/ai/AiOutlineMail'
-import { TiSocialInstagramCircular } from '@react-icons/all-files/ti/TiSocialInstagramCircular'
+import SOCIAL_PROFILES from '../content/SocialProfiles'
+import Icon from './SvgIcon'
 
 const Links = () => (
-  <div className="flex justify-items-end mt-3 text-theme-orange">
-    <div className="ml-44" />
-    <AiFillGithub size={18} />
-    <AiFillLinkedin size={18} />
-    <AiFillTwitterCircle size={18} />
-    <AiOutlineMail size={18} />
-    <TiSocialInstagramCircular size={18} />
+  <div className="flex justify-items-end">
+    {SOCIAL_PROFILES.map( item => <Icon url={item.url} svg={item.icon} /> )}
   </div>
-
 )
 
 export default Links
---
+++ src/content/SocialProfiles.js
@@ -0,0 +1,30 @@
+import { AiFillGithub } from '@react-icons/all-files/ai/AiFillGithub'
+import { AiFillLinkedin } from '@react-icons/all-files/ai/AiFillLinkedin'
+import { AiFillTwitterCircle } from '@react-icons/all-files/ai/AiFillTwitterCircle'
+import { AiOutlineMail } from '@react-icons/all-files/ai/AiOutlineMail'
+import { TiSocialInstagramCircular } from '@react-icons/all-files/ti/TiSocialInstagramCircular'
+
+const SOCIAL_PROFILES = [
+  {
+    url: 'https://github.com/gelicamarie',
+    icon: AiFillGithub,
+  },
+  {
+    url: 'https://www.linkedin.com/in/angelica-turla',
+    icon: AiFillLinkedin,
+  },
+  {
+    url: 'https://twitter.com/gxlica',
+    icon: AiFillTwitterCircle,
+  },
+  {
+    url: 'mailto:turla_angelica@yahoo.com',
+    icon: AiOutlineMail,
+  },
+  {
+    url: 'https://www.instagram.com/gelica.marie',
+    icon: TiSocialInstagramCircular,
+  },
+]
+
+export default SOCIAL_PROFILES
