9d01d1d1eeac306b0bfdf5dbe52c93cf0b1ab5c8
gelicamarie <turla_angelica@yahoo.com>
2021-05-12 23:16:28 -0400

    About Section Animation Mobile Fixed

--- src/__generated__/gatsby-types.ts
+++ src/__generated__/gatsby-types.ts
@@ -257,6 +257,8 @@ type Directory_ctimeArgs = {
 type Site = Node & {
   readonly buildTime: Maybe<Scalars['Date']>;
   readonly siteMetadata: Maybe<SiteSiteMetadata>;
+  readonly port: Maybe<Scalars['Int']>;
+  readonly host: Maybe<Scalars['String']>;
   readonly polyfill: Maybe<Scalars['Boolean']>;
   readonly pathPrefix: Maybe<Scalars['String']>;
   readonly id: Scalars['ID'];
@@ -822,6 +824,8 @@ type Query_allDirectoryArgs = {
 type Query_siteArgs = {
   buildTime: Maybe<DateQueryOperatorInput>;
   siteMetadata: Maybe<SiteSiteMetadataFilterInput>;
+  port: Maybe<IntQueryOperatorInput>;
+  host: Maybe<StringQueryOperatorInput>;
   polyfill: Maybe<BooleanQueryOperatorInput>;
   pathPrefix: Maybe<StringQueryOperatorInput>;
   id: Maybe<StringQueryOperatorInput>;
@@ -1765,6 +1769,8 @@ type SiteFieldsEnum =
   | 'siteMetadata.title'
   | 'siteMetadata.description'
   | 'siteMetadata.author'
+  | 'port'
+  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'id'
@@ -1866,6 +1872,8 @@ type SiteGroupConnection = {
 type SiteFilterInput = {
   readonly buildTime: Maybe<DateQueryOperatorInput>;
   readonly siteMetadata: Maybe<SiteSiteMetadataFilterInput>;
+  readonly port: Maybe<IntQueryOperatorInput>;
+  readonly host: Maybe<StringQueryOperatorInput>;
   readonly polyfill: Maybe<BooleanQueryOperatorInput>;
   readonly pathPrefix: Maybe<StringQueryOperatorInput>;
   readonly id: Maybe<StringQueryOperatorInput>;
@@ -3060,15 +3068,15 @@ type SitePluginSortInput = {
   readonly order: Maybe<ReadonlyArray<Maybe<SortOrderEnum>>>;
 };
 
+type PagesQueryQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type PagesQueryQuery = { readonly allSiteFunction: { readonly nodes: ReadonlyArray<Pick<SiteFunction, 'apiRoute'>> }, readonly allSitePage: { readonly nodes: ReadonlyArray<Pick<SitePage, 'path'>> } };
+
 type SeoMetadataQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 type SeoMetadataQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'description'>> }> };
-
-type HomepageQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type HomepageQuery = { readonly collage: Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }>, readonly bkg: Maybe<{ readonly childImageSharp: Maybe<Pick<ImageSharp, 'gatsbyImageData'>> }> };
 
 type GatsbyImageSharpFixedFragment = Pick<ImageSharpFixed, 'base64' | 'width' | 'height' | 'src' | 'srcSet'>;
 
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,5 @@
 import Typist from 'react-typist'
+import { useWindowWidth } from '@react-hook/window-size'
 import { Animator, ScrollContainer, ScrollPage, batch, Fade, MoveOut, Sticky } from 'react-scroll-motion'
 
 import Link from '../Link'
@@ -6,39 +7,53 @@ import SEO from '../Seo'
 
 import './index.css'
 
-const About = () => (
-  <>
-    <SEO title="About Me" />
-    <main id="about" className="about">
-      <ScrollContainer>
-        <ScrollPage page={1}>
-          <Animator animation={batch( Fade(), Sticky(), MoveOut( 0, -900 ) )}>
-            <div className="container">
+const MobileHeading = () => (
+  <Typist avgTypingDelay={85}>
+    <Typist.Delay ms={1700} />
+    <span>  Computer <br />Programmer</span>
+    <Typist.Backspace count={21} delay={400} />
+    <span>Developer </span>
+  </Typist>
+)
 
-              <div className="heading">
-                <Typist avgTypingDelay={85}>
-                  <Typist.Delay ms={1700} />
-                  <span>Computer Programmer</span>
-                  <Typist.Backspace count={20} delay={400} />
-                  <span>Developer </span>
-                </Typist>
+const DesktopHeading = () => (
+  <Typist avgTypingDelay={85}>
+    <Typist.Delay ms={1700} />
+    <span>Computer Programmer</span>
+    <Typist.Backspace count={21} delay={400} />
+    <span>Developer </span>
+  </Typist>
+)
+const About = () => {
+  const width = useWindowWidth()
+  const Heading = () => ( width < 769 ? <MobileHeading /> : <DesktopHeading /> )
+  return (
+    <>
+      <SEO title="About Me" />
+      <main id="about" className="about">
+        <ScrollContainer>
+          <ScrollPage page={1}>
+            <Animator animation={batch( Fade(), Sticky(), MoveOut( 0, -900 ) )}>
+              <div className="container">
 
-              </div>
+                <div className="heading">
+                  <Heading />
+                </div>
 
-              <div className="grid-container">
-                <div>
-                  <h4>Hello, <br />
+                <div className="grid-container">
+                  <div>
+                    <h4>Hello, <br />
                       I’m Angelica Turla
                     </h4>
-                  <p>
+                    <p>
                       I am currently on my 2nd year of Computer Science at Carleton University.
                       <br />
                       <br />
                     </p>
-                </div>
+                  </div>
 
-                <div>
-                  <p>
+                  <div>
+                    <p>
                       Still finding my footing in the vast world of technology and so I always
                       keep an open mind when it comes to learning new things.
                       <br />
@@ -46,13 +61,14 @@ const About = () => (
                       I work on a variety of technologies and frameworks including but not limited
                       to Python, Java, C/C++, JavaScript, and React. View more <Link to="/#skills">here</Link>.
                     </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Animator>
-        </ScrollPage>
-      </ScrollContainer>
-    </main>
-  </>
-)
+            </Animator>
+          </ScrollPage>
+        </ScrollContainer>
+      </main>
+    </>
+  )
+}
 export default About
